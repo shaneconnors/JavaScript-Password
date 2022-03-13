6613dc17394d7aa12bc1940c024d2a07de38378f
Shane Connors <shaneconnors1992@gmail.com>
2022-03-12 21:04:00 -0800

    checking get user choices

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,13 @@
 // Assignment Code
 
-//Get reference to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-function randomPickChar(text, numberChars = 1) {
-  let randomString = "";
-
-  for (let i = 0; i < numberChars; i++) {
-    randomString += text.charAt(Math.floor(Math.random() * text.length));
-  }
-
-  return randomString;
-}
-
-
 var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialChar = `!"#$%&'()*+,-./:;<=>?@[\]^_\`{|}~`;
 var passwordLength;
+
+//Get reference to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -28,7 +17,7 @@ function generatePassword() {
   passwordLength = prompt("How many characters would you like in your password? Choose between 8 and 128");
   console.log("Password length " + passwordLength);
   
-    if(!passwordLength) {
+    if(passwordLength) {
     alert("This field is required");
   } if (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Your password must be between 8 and 128 characters");
@@ -52,7 +41,7 @@ function generatePassword() {
 let getUserChoices = getUserPreferences(); 
 console.log(getUserChoices)
 if (getUserChoices.getNumber == true) { window.prompt (numberChar)
-}
+} else (getUserChoices.getNumber == false) 
 if (getUserChoices.getSpecialChar == true) { window.prompt (upperChar) 
 }
 if (getUserChoices.getLowerChar == true) { window.prompt (lowerChar)
