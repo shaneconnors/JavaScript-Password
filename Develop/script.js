cfb3e376f705328843cacde052e6e1a0b2220a5c
Shane Connors <shaneconnors1992@gmail.com>
2022-03-13 11:35:23 -0800

    added empty array and other variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,33 @@
 // Assignment Code
 
-var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = `!"#$%&'()*+,-./:;<=>?@[\]^_\`{|}~`;
-var passwordLength;
+var numberCharArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperCharArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCharArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharArray = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var passwordTotal = [];
+var passwordArray = [];
 
+var passwordLength;
+var numberChar;
+var upperChar;
+var lowerChar;
+var specialChar;
 //Get reference to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 function generatePassword() {
+   
   // Ask for user Input
   passwordLength = prompt("How many characters would you like in your password? Choose between 8 and 128")
   
@@ -25,51 +40,28 @@ function generatePassword() {
     return 
 
   }
+
+  let numberChar = confirm("Do you want numbers in your password?"); 
+
   
-function determineanswers() {}
-  // ask all the prompts
-  let getNumber = confirm("Do you want numbers in your password?"); 
-  if(getNumber) {
-    numberChar
-  }
-  let getUpperChar = confirm("Do you want upper case characters in your password?");
-  if(getUpperChar) {
-    upperChar
-  }
-  let getLowerChar = confirm("Do you want lower case characters in your password?");
-  if(getLowerChar) {
-    lowerChar
-  }
-  let getSpecialChar = confirm("Do you want special characters in your password?"); 
-  if(getSpecialChar) {
-    specialChar
-  }
+  let  upperChar = confirm("Do you want upper case characters in your password?");
 
-  return {
-    getNumber,
-    getUpperChar,
-    getLowerChar,
-    getSpecialChar
-  }
-}
-
-let getUserChoices = getUserPreferences(); 
-console.log(getUserChoices)
-if (getUserChoices.getNumber == true) { window.prompt (numberChar)
-} else (getUserChoices.getNumber == false) 
-if (getUserChoices.getSpecialChar == true) { window.prompt (upperChar) 
-}
-if (getUserChoices.getLowerChar == true) { window.prompt (lowerChar)
-}
-if (getUserChoices.getSpecialChar == true) { window.prompt (specialChar)
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
   
-  passwordText.value = password;
+  let lowerChar = confirm("Do you want lower case characters in your password?");
+
+  
+  let specialChar = confirm("Do you want special characters in your password?"); 
+
+
+  
+  for (var i = 0; i < passwordLength; i++) {
+    var randomnumber = passwordTotal[Math.floor(Math.random()*passwordTotal.length)];
+    passwordArray = passwordArray & (randomnumber);
+  }
+
+  var password = passwordArray.join('');
+  return password;
+
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
